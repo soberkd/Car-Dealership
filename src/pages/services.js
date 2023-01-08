649b161e407b3aca45615d582c35833d2eb7edb5
soberkd <soberkd@gmail.com>
2023-01-08 18:58:25 +0300

    Update services

--- src/pages/services.js
+++ src/pages/services.js
@@ -6,6 +6,20 @@ export default function Services() {
     <Layout>
         <h4>Services</h4>
         <p>We offer customer services to our clients</p>
+        <p>1. Service and maintenance: Most dealerships have a service center on site where customers can bring 
+            their vehicles for routine maintenance and repairs. This can include oil changes, tire rotations, 
+            and other regular upkeep, as well as more extensive repairs.</p>
+        <p>2. Parts and accessories: Dealerships often have a parts department where customers can purchase OEM 
+            (original equipment manufacturer) parts and accessories for their vehicles.</p>
+        <p>3. Detailing: Many dealerships offer detailing services to help customers keep their vehicles looking 
+            clean and well-maintained. This can include interior and exterior detailing, as well as services 
+            like waxing and buffing.</p>
+        <p>4. Car rental: Some dealerships have rental car departments where customers can rent vehicles for 
+            short periods of time.</p>
+        <p>5. Trade-ins: Many dealerships will allow customers to trade in their old vehicles when purchasing a 
+            new one, which can be a convenient way for customers to get value for their old car.</p>
+        <p>6. Leasing: In addition to selling vehicles, some dealerships also offer leasing options for customers 
+            who prefer to rent a car for a longer period of time.</p>
     </Layout>
   )
 }
